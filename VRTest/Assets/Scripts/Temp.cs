5e4ce06f67be0ceaf0340677f9094c5bdb119dce
Matthew Mok <matthewmoktf@gmail.com>
2020-04-07 14:23:32 +0800

    Update Temp.cs

--- VRTest/Assets/Scripts/Temp.cs
+++ VRTest/Assets/Scripts/Temp.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Temp : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int Hp = 60;
+    private bool isKilled = false;
+
     void Start()
     {
         
+    
     }
 
     // Update is called once per frame
